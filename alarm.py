f6849b98f9e5d1a0b261e125b40c4caebc6fecd9
joycelin570958-collab <joycelin570958@gmail.com>
2026-07-30 09:03:52 -0400

    adding get request to fivetests

--- alarm.py
+++ alarm.py
@@ -13,7 +13,7 @@ user_stats = {}
 def homepage():
     return render_template("homepage.html")
 
-@app.route("/fivetests", methods=["POST"])
+@app.route("/fivetests", methods=["GET","POST"])
 def fivetests():
     input_username = request.form.get("user_name", "")
     selected_sound = request.form.get("alarm-sound", "")
